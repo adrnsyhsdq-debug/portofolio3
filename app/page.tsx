69885012b007185a87ef20e63fd5edbe7271b082
Crafted <adrnsyhsdq@gmail.com>
2026-07-14 13:20:49 +0700

    feat: activate ColorBar as a section divider on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { Hero } from '@/components/hero'
 import { Services } from '@/components/services'
 import { CTA } from '@/components/cta'
 import { Footer } from '@/components/footer'
+import { ColorBar } from '@/components/color-bar'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ColorBar className="opacity-20" />
       <Services />
       <CTA />
       <Footer />
